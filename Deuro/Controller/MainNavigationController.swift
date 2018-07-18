c6a6b5b49580aa8c56ba9990de9c86459353ac54
Grey Chen <957024640@qq.com>
2018-07-18 13:01:12 -0700

    add activaty loading indicator

--- Deuro/Controller/MainNavigationController.swift
+++ Deuro/Controller/MainNavigationController.swift
@@ -10,34 +10,32 @@ import UIKit
 import AXWebViewController
 
 class MainNavigationController: UINavigationController {
-
+    let indicator: UIActivityIndicatorView = UIActivityIndicatorView(activityIndicatorStyle: UIActivityIndicatorViewStyle.gray)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         let webVC = AXWebViewController(address: "http://ai.deuro.io")
         webVC.showsToolBar = true
+        webVC.delegate = self
         webVC.navigationType = .toolItem
         webVC.navigationController?.navigationBar.isTranslucent = false
         navigationController?.navigationBar.tintColor = UIColor(red: 0.100, green: 0.100, blue: 0.100, alpha: 0.800)
         navigationController?.navigationBar.barTintColor = UIColor(red: 0.996, green: 0.867, blue: 0.522, alpha: 1.00)
         navigationController?.pushViewController(webVC, animated: true)
         self.setViewControllers([webVC], animated: true)
+        
+        
+        indicator.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+        indicator.center = view.center
+        self.view.addSubview(indicator)
+        self.view.bringSubview(toFront: indicator)
+        indicator.startAnimating()
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+extension MainNavigationController: AXWebViewControllerDelegate {
+    func webViewControllerDidFinishLoad(_ webViewController: AXWebViewController) {
+        indicator.stopAnimating()
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
