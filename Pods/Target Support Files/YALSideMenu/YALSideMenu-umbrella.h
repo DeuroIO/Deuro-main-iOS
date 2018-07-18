c8777846716082b4360ce6dd5941c6ba8f58aaa5
Grey Chen <957024640@qq.com>
2018-07-18 09:07:29 -0700

    change the project architecture

--- Pods/Target Support Files/YALSideMenu/YALSideMenu-umbrella.h
+++ Pods/Target Support Files/YALSideMenu/YALSideMenu-umbrella.h
@@ -11,6 +11,6 @@
 #endif
 
 
-FOUNDATION_EXPORT double SwiftWebVCVersionNumber;
-FOUNDATION_EXPORT const unsigned char SwiftWebVCVersionString[];
+FOUNDATION_EXPORT double SideMenuVersionNumber;
+FOUNDATION_EXPORT const unsigned char SideMenuVersionString[];
 
